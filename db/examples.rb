3c728ce1e1359d7ee1ae6a0becaaed9dd93b1a6d
kopiusmaximus <alexander.kopynec@gmail.com>
2016-11-08 17:40:10 -0500

    Uncomment selected examples for deployment

--- db/examples.rb
+++ db/examples.rb
@@ -1,21 +1,5 @@
-# This file should contain all the record creation needed to experiment with
-# your app during development.
-#
-# The data can then be loaded with the rake db:examples (or created alongside
-# the db with db:nuke_pave).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# %w(antony jeff matt jason).each do |name|
-#   email = "#{name}@#{name}.com"
-#   next if User.exists? email: email
-#   User.create!(email: email,
-#                password: 'abc123',
-#                password_confirmation: nil)
-# end
+# This file containst all the record creation needed to experiment with
+# the app during development.
 
 User.create(email: 'sunny@sunny.com', username: 'Sunny', password: '1')
 
@@ -37,44 +21,44 @@ Affirmation.create([
                      }
                    ])
 
-Morning.create(user_id: 1)
-
-MorningAffirmation.create([
-                            {
-                              completed: false,
-                              morning_id: 1,
-                              affirmation_id: 1
-                            },
-                            {
-                              completed: false,
-                              morning_id: 1,
-                              affirmation_id: 2
-                            },
-                            {
-                              completed: false,
-                              morning_id: 1,
-                              affirmation_id: 3
-                            }
-                          ])
-
-# Create a MorningAffirmation with no morning
-MorningAffirmation.create([
-                            {
-                              completed: false,
-                              affirmation_id: 1
-                            }
-                          ])
-
-# Create a MorningAffirmation with a different morning
-Morning.create(user_id: 1)
-MorningAffirmation.create([
-                            {
-                              completed: true,
-                              morning_id: 2,
-                              affirmation_id: 1
-                            }
-                          ])
-
+# Morning.create(user_id: 1)
+#
+# MorningAffirmation.create([
+#                             {
+#                               completed: false,
+#                               morning_id: 1,
+#                               affirmation_id: 1
+#                             },
+#                             {
+#                               completed: false,
+#                               morning_id: 1,
+#                               affirmation_id: 2
+#                             },
+#                             {
+#                               completed: false,
+#                               morning_id: 1,
+#                               affirmation_id: 3
+#                             }
+#                           ])
+#
+# # Create a MorningAffirmation with no morning
+# MorningAffirmation.create([
+#                             {
+#                               completed: false,
+#                               affirmation_id: 1
+#                             }
+#                           ])
+#
+# # Create a MorningAffirmation with a different morning
+# Morning.create(user_id: 1)
+# MorningAffirmation.create([
+#                             {
+#                               completed: true,
+#                               morning_id: 2,
+#                               affirmation_id: 1
+#                             }
+#                           ])
+#
 User.create(email: 'grumpy@grumpy.com', username: 'Grumpy', password: '1')
 
 Affirmation.create([
@@ -86,11 +70,11 @@ Affirmation.create([
                    ])
 
 Morning.create(user_id: 2)
-
-MorningAffirmation.create([
-                            {
-                              completed: false,
-                              morning_id: 3,
-                              affirmation_id: 4
-                            }
-                          ])
+#
+# MorningAffirmation.create([
+#                             {
+#                               completed: false,
+#                               morning_id: 3,
+#                               affirmation_id: 4
+#                             }
+#                           ])
